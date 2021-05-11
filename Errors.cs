255c24c0ed3f54a0382bdfcd71cb91c319425d08
Fr3myXS <antonovch13@mail.ru>
2021-05-11 18:54:43 +0500

    Update.Wpf 2

--- Errors.cs
+++ Errors.cs
@@ -27,6 +27,12 @@ namespace AdvancedCalculate.WPF
 
                     return false;
                 }
+                else if(function.Contains('/') && function[function.IndexOf('/') + 1] == '0')
+                {
+                    GetError(function, i);
+
+                    return false;
+                }
             }
 
             return true;
@@ -52,17 +58,30 @@ namespace AdvancedCalculate.WPF
                 GetError(function, '(');
                 return false;
             }
+            else if(function.Count(e => e == ')') == function.Count(e => e == '('))
+            {
+                if (function.IndexOf('(') > function.IndexOf(')'))
+                {
+                    GetError(function, '(');
+                    return false;
+                }
+                else if (function.IndexOf(')') == function.IndexOf('(') + 1)
+                {
+                    GetError(function, ')');
+                    return false;
+                }
+            }
             return true;
         }
         private static void GetError(string function, char ind)
         {
-            string error = function + "\n" + new string(' ', function.IndexOf(ind) + 1) + '^';
+            string error = function + "\n" + new string(' ', function.IndexOf(ind) + 2) + '^';
             MessageBox.Show(error);
         }
 
         public static bool IsStep(string value)
         {
-            if((value.Length <= 0 ))
+            if(value.Length <= 0  || !IsNumber(value))
             {
                 MessageBox.Show("Неккоректный ввод данных");
                 return false;
@@ -72,11 +91,34 @@ namespace AdvancedCalculate.WPF
         }
         private static bool IsNumber(string value)
         {
-
             foreach (var i in value)
             {
-                if (i < '0' || i > '9')
+                if (i <'0' || i > '9')
                 {
+                    if (i != '-')
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+        public static bool CheckRange(string stepX, string startX, string endX)
+        {
+            if(int.Parse(stepX) > 0)
+            {
+                if(int.Parse(startX) >= int.Parse(endX))
+                {
+                    MessageBox.Show("Неккоректный ввод данных");
+                    return false;
+                }
+            }
+            else if(int.Parse(stepX) < 0)
+            {
+                if (int.Parse(startX) <= int.Parse(endX))
+                {
+                    MessageBox.Show("Неккоректный ввод данных");
                     return false;
                 }
             }
